79acd4c2b80b5c2a4990d1c24fa2d8ee7b417491
Dongyun <dyunzero@gmail.com>
2020-07-10 15:06:25 +0900

    스케줄러는 node 바깥에서 돌리도록 변경

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import schedule from 'node-schedule';
 import Send from './Send';
 
 (async() => {
-    if(process.env.NODE_ENV === 'debug') {
-        await Send();
-    } else {
-        schedule.scheduleJob('0 0,15 * * *', async () => {
-            await Send();
-        });
-    }
+    await Send();
 })();
